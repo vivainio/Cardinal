d51d1b9844f8b8fd4d421159b9e6baf0193efc80
Ville M. Vainio <ville.ma.vainio@nokia.com>
2011-03-15 10:57:40 +0200

    sync leo file

--- cardinal.py
+++ cardinal.py
@@ -7,13 +7,13 @@ import Ui_ProcLauncher
 
 class RRunner(QtCore.QThread):
     def __init__(self, f, parent = None):
-    
+
         QtCore.QThread.__init__(self, parent)
         self.f = f
-                
+
     def run(self):
         self.res = self.f()
-    
+
 
 class ProcLauncher(QtGui.QMainWindow):
     def __init__(self, parent=None):
@@ -25,26 +25,26 @@ class ProcLauncher(QtGui.QMainWindow):
             self.ses.connect()
         except:
             pass
-            
+
         self.ui.bLaRun.clicked.connect(self.do_run)
         self.ui.inpProcName.setText("/usr/bin/widgetsgallery")
         self.add_actions()
         self.procs = []
     def add_actions(self):
         self.ui.actionSetup_device.triggered.connect(self.setup_device)
-        
+
     def setup_device(self):
         print "setup"
         self.ses.copykey()
         self.ses.setup_remote()
-    
+
     def do_cmd(self, cmd):
         def run():
             return self.ses.ex_full(cmd)
         r = RRunner(run)
         r.start()
         self.procs.append(r)
-        
+
     def do_run(self):
         cmd = self.ui.inpProcName.text()
         print "run", cmd
@@ -55,13 +55,13 @@ class ProcLauncher(QtGui.QMainWindow):
         cmd2 = "strace -o {SDIR}/strace %s" % (cmd,)
         print "strace", cmd2
         self.do_cmd(cmd2)
-        
-        
-        
-        
+
+
+
+
     def do_find(self):
         pass
-        
+
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
