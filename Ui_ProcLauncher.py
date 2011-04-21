8d3a3d9ab8e35681abe7049b37c2918dbaaac8d6
Ville M. Vainio <ville.ma.vainio@nokia.com>
2011-04-21 09:05:45 +0300

    windowtitle

--- Ui_ProcLauncher.py
+++ Ui_ProcLauncher.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'proclauncher.ui'
 #
-# Created: Thu Apr 21 08:35:12 2011
+# Created: Thu Apr 21 09:05:28 2011
 #      by: PyQt4 UI code generator 4.7.4
 #
 # WARNING! All changes made in this file will be lost!
